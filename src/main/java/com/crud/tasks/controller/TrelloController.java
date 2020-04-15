cc9468d233628533ff298afe6433285dd746c493
UserWeronikaGarbarz <weronika.garbarz@gmail.com>
2020-04-15 18:35:08 +0200

    Task 18.2

---
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -0,0 +1,45 @@
+package com.crud.tasks.controller;
+
+import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.domain.TrelloCardDto;
+import com.crud.tasks.mapper.CreatedTrelloCard;
+import com.crud.tasks.trello.client.TrelloClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/trello")
+public class TrelloController {
+
+    @Autowired
+    private TrelloClient trelloClient;
+
+
+    @RequestMapping(method = RequestMethod.GET, value = "getTrelloBoards")
+    public void getTrelloBoards() {
+
+        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
+        System.out.println(trelloBoards);
+
+        trelloBoards.stream().filter((x) -> trelloBoards.contains("(.*)name(.*)") && trelloBoards.contains("(.*)id(.*)"))
+                .filter(trelloBoardDto -> trelloBoardDto.getName().matches("Kodilla(.*)"))
+                .forEach(trelloBoardDto -> System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName()));
+
+        // GET request
+        trelloBoards.forEach(trelloBoardDto -> {
+            System.out.println(trelloBoardDto.getName() + " - " + trelloBoardDto.getId());
+            System.out.println("This board contains lists: ");
+            trelloBoardDto.getLists().forEach(trelloList ->
+                    System.out.println(trelloList.getName() + " - " + trelloList.getId() + " - " + trelloList.isClosed()));
+        });
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "createTrelloCard")
+    public CreatedTrelloCard createTrelloCard(@RequestBody TrelloCardDto trelloCardDto) {
+
+        return trelloClient.createNewCard(trelloCardDto);
+    }
+}
